86f6e6c41a17adfe8334ebe4a654fbbf44c24374
Stephen Borsay <borsay@gmail.com>
2020-07-11 16:22:26 -0700

    Disptahces incoming JSON IoT event from IoT Core to S3

---
+++ lambda/IoTtoS3_Dispatch.js
@@ -0,0 +1,21 @@
+        var AWS = require('aws-sdk');
+        var s3 = new AWS.S3();
+    
+        exports.handler = (event, context, callback) => {  
+            var bucketName = "parkdensitydata/iotdata";
+            var keyName = JSON.stringify(Date.now());
+            var content = JSON.stringify(event);                        //uncomment this statement for testing in lambda
+          //var content = JSON.stringify(event.queryStringParameters);  //uncommnet this statement after integration with API Gateway
+                                                                        //keep only one of the above uncommented!
+        
+        
+            var params = { Bucket: bucketName, Key: keyName, Body: content};
+        
+            s3.putObject(params, function (err, data) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("Successfully saved object to " + bucketName + "/" + keyName 
+                                                                + "and data=" + JSON.stringify(content));
+            });
+        };
