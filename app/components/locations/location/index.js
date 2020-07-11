b03390f313f050bfad076e8345c0f4009aa75c18
Mark David Avery <mark@webark.cc>
2020-07-11 16:09:45 -0700

    fix: polishing some things up

--- app/components/locations/location/index.js
+++ app/components/locations/location/index.js
@@ -1,18 +1,10 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 
 export default class LocationsLocationComponent extends Component {
   styleNamespace = '__locations__location';
 
-  densityColors = [
-    '#0CE500',
-    '#3CE000',
-    '#6ADC00',
-    '#97D800',
-    '#C1D400',
-    '#CFB500',
-    '#CB8500',
-    '#C75700',
-    '#C32C00',
-    '#BF0100',
-  ];
+  get color() {
+    return htmlSafe(`color: ${this.args.location.density.color}`);
+  }
 }
---
+++ app/routes/application.js
@@ -0,0 +1,57 @@
+import Route from '@ember/routing/route';
+
+import geoLocations from 'healthy-life/data/parks';
+
+function formatCordinates(shape) {
+  return shape.map(function([lng, lat]) {
+    return { lat, lng, };
+  });
+}
+
+function getShapes(coordinates) {
+  return coordinates.map(function(shapes) {
+    if (!Array.isArray(shapes[0][0])) {
+      shapes = [shapes];
+    }
+    return shapes.map(formatCordinates)[0];
+  })
+}
+
+function getRandomIntRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const densityColors = [
+  '#0CE500',
+  '#3CE000',
+  '#6ADC00',
+  '#97D800',
+  '#C1D400',
+  '#CFB500',
+  '#CB8500',
+  '#C75700',
+  '#C32C00',
+  '#BF0100',
+];
+
+
+const locations = geoLocations.features.map(feature => {
+  const density = getRandomIntRange(1, 10);
+  return {
+    shapes: getShapes(feature.geometry.coordinates),
+    id: feature.properties.OBJECTID,
+    name: feature.properties.NAME,
+    density: {
+      number: density,
+      color: densityColors[density - 1],
+    }
+  }
+});
+
+export default class ApplicationRoute extends Route {
+  model() {
+    return locations;
+  }
+}
